5a68dbd7e5c8bc3510b3a76823bf2bb87edb5044
Nubelson Fernandes <nubelsondev@hotmail.com>
2023-01-28 11:49:56 +0000

    fix(Design System): change title line-height

--- src/components/shared/atoms/Titles/styles.ts
+++ src/components/shared/atoms/Titles/styles.ts
@@ -3,35 +3,35 @@ import styled from "styled-components";
 export const StyledTitleLarge = styled.h1`
   color: ${(props) => props.theme.colors.title};
   font-size: 6.4rem;
-  line-height: 66px;
+  line-height: 72px;
   font-weight: 700;
 
   @media ${(props) => props.theme.mediaQueries.medium_s} {
     font-size: 3.6rem;
-    line-height: 36px;
+    line-height: 42px;
   }
 `;
 
 export const StyledTitleMedium = styled.h2`
   color: ${(props) => props.theme.colors.title};
   font-size: 4.8rem;
-  line-height: 50px;
+  line-height: 56px;
   font-weight: 700;
 
   @media ${(props) => props.theme.mediaQueries.medium_s} {
     font-size: 2.4rem;
-    line-height: 32px;
+    line-height: 38px;
   }
 `;
 
 export const StyledTitleSmall = styled.h3`
   color: ${(props) => props.theme.colors.title};
   font-size: 3.2rem;
-  line-height: 34px;
+  line-height: 40px;
   font-weight: 700;
 
   @media ${(props) => props.theme.mediaQueries.medium_s} {
     font-size: 2.2rem;
-    line-height: 32px;
+    line-height: 38px;
   }
 `;
