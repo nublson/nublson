5fa73086b8d2a314d3357da2c01979d965fc32f7
Nubelson Fernandes <nubelsondev@hotmail.com>
2023-07-21 15:02:45 +0100

    feat(Share): add url to share

--- src/sections/Share/index.tsx
+++ src/sections/Share/index.tsx
@@ -1,4 +1,7 @@
-import { PrimaryIcon } from "@/components/shared/Buttons";
+"use client";
+
+import { PrimaryIcon, PrimaryText } from "@/components/shared/Buttons";
+import { usePathname } from "next/navigation";
 import { RiLinkedinLine, RiTwitterLine, RiWhatsappLine } from "react-icons/ri";
 import styles from "./styles.module.scss";
 
@@ -7,22 +10,54 @@ interface ShareSectionProps {
 }
 
 export const ShareSection = (props: ShareSectionProps) => {
+  const pathname = usePathname();
+
+  const generateShareLink = (source: string, path: string) => {
+    const url = `${process.env.BASE_URL}${path}`;
+
+    if (source === "twitter") {
+      return `https://twitter.com/intent/tweet?url=${url}`;
+    } else if (source === "whatsapp") {
+      return `https://wa.me/?text=${url}`;
+    } else {
+      return `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.info}>{props.children}</div>
       <div className={styles.share}>
-        <PrimaryIcon
-          size="small"
-          icon={<RiTwitterLine className={styles.icon} />}
-        />
-        <PrimaryIcon
-          size="small"
-          icon={<RiWhatsappLine className={styles.icon} />}
-        />
-        <PrimaryIcon
-          size="small"
-          icon={<RiLinkedinLine className={styles.icon} />}
-        />
+        <a
+          href={generateShareLink("twitter", pathname)}
+          target="_blank"
+          rel="noopener"
+        >
+          <PrimaryIcon
+            size="small"
+            icon={<RiTwitterLine className={styles.icon} />}
+          />
+        </a>
+        <a
+          href={generateShareLink("whatsapp", pathname)}
+          target="_blank"
+          rel="noopener"
+        >
+          <PrimaryIcon
+            size="small"
+            icon={<RiWhatsappLine className={styles.icon} />}
+          />
+        </a>
+        <a
+          href={generateShareLink("linkedIn", pathname)}
+          target="_blank"
+          rel="noopener"
+        >
+          <PrimaryIcon
+            size="small"
+            icon={<RiLinkedinLine className={styles.icon} />}
+          />
+        </a>
       </div>
     </div>
   );
