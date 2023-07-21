4763c6956cf84d107ba7068e2848754b57c3e32f
Nubelson Fernandes <nubelsondev@hotmail.com>
2023-07-21 09:11:50 +0100

    feat(Responsive): add Laptop font-sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Poppins } from "next/font/google";
 import "./globals.scss";
-import Script from "next/script";
 
 import { Layout } from "@/components/Layout";
 import { Footer } from "@/components/shared/Footer";
--- src/sections/Share/index.tsx
+++ src/sections/Share/index.tsx
@@ -11,9 +11,18 @@ export const ShareSection = (props: ShareSectionProps) => {
     <div className={styles.container}>
       <div className={styles.info}>{props.children}</div>
       <div className={styles.share}>
-        <PrimaryIcon size="small" icon={<RiTwitterLine size={"20px"} />} />
-        <PrimaryIcon size="small" icon={<RiWhatsappLine size={"20px"} />} />
-        <PrimaryIcon size="small" icon={<RiLinkedinLine size={"20px"} />} />
+        <PrimaryIcon
+          size="small"
+          icon={<RiTwitterLine className={styles.icon} />}
+        />
+        <PrimaryIcon
+          size="small"
+          icon={<RiWhatsappLine className={styles.icon} />}
+        />
+        <PrimaryIcon
+          size="small"
+          icon={<RiLinkedinLine className={styles.icon} />}
+        />
       </div>
     </div>
   );
