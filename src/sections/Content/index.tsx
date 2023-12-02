86ae171e50ab4e39eab752b836ec25d8ab58a77f
Nubelson Fernandes <nubelsondev@hotmail.com>
2023-12-02 14:28:37 +0000

    fix: remove image pre processor

--- src/sections/Content/index.tsx
+++ src/sections/Content/index.tsx
@@ -11,31 +11,7 @@ export const ContentSection = ({ blocks }: ContentSectionProps) => {
   return (
     <section className={styles.container}>
       <div className={styles.blocks}>
-        <Render
-          blocks={blocks}
-          simpleTitles
-          classNames
-          blockComponentsMapper={{
-            image: withContentValidation(async (props) => {
-              if (props.media && props.media.src) {
-                const { base64, img } = await getRemoteImage(props.media.src);
-
-                return (
-                  <Image
-                    src={img.src}
-                    alt={props.media.alt}
-                    width={img.width}
-                    height={img.height}
-                    placeholder="blur"
-                    blurDataURL={base64}
-                  />
-                );
-              } else {
-                return null;
-              }
-            }),
-          }}
-        />
+        <Render blocks={blocks} simpleTitles classNames />
       </div>
     </section>
   );
