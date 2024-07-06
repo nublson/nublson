34162463dc1130a8e9fec771c4bdeb89a9244fc3
Nubelson Fernandes <nubelsondev@hotmail.com>
2024-07-06 20:19:18 +0100

    feat: add store pages sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,19 +2,29 @@ import { getData } from "@/services/notion";
 import sitemapList from "@/utils/sitemapPages.json";
 
 export default async function sitemap() {
-  const articles = (await getData(process.env.NOTION_DATABASE_ARTICLES_ID)).map(
-    (item) => {
-      return {
-        url: `${process.env.BASE_URL}/blog/${item.post_slug}`,
-        lastModified: item.modified_date,
-      };
-    }
-  );
+  const [articles, products] = await Promise.all([
+    getData(process.env.NOTION_DATABASE_ARTICLES_ID),
+    getData(process.env.NOTION_DATABASE_PRODUCTS_ID),
+  ]);
+
+  const blogSitemaps = articles.map((item) => {
+    return {
+      url: `${process.env.BASE_URL}/blog/${item.post_slug}`,
+      lastModified: item.modified_date,
+    };
+  });
+
+  const storeSitemaps = products.map((item) => {
+    return {
+      url: `${process.env.BASE_URL}/store/${item.post_slug}`,
+      lastModified: item.modified_date,
+    };
+  });
 
   const routes = sitemapList.pages.map((item) => ({
     url: `${process.env.BASE_URL}${item.path}`,
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes, ...articles];
+  return [...routes, ...blogSitemaps, ...storeSitemaps];
 }
