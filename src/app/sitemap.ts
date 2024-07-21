e1cf974dacf583d92440c9b633cc6a7e205ffe4e
Nubelson Fernandes <nubelsondev@hotmail.com>
2024-07-21 18:15:29 +0100

    fix: sitemapGenerator

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,11 @@
-import { getData } from "@/services/notion";
-import sitemapList from "@/utils/sitemapPages.json";
+import { getData } from "@/services/notion"; // Adjust the import path
+import sitemapList from "@/utils/sitemapPages.json"; // Adjust the import path
+import { PostProps } from "@/utils/types"; // Adjust the import path
 
-async function fetchAllPages(databaseId: string) {
+async function fetchAllPages(databaseId: string): Promise<PostProps[]> {
   let page = 1;
   let hasMore = true;
-  const allPosts = [];
+  const allPosts: PostProps[] = [];
 
   while (hasMore) {
     const data = await getData(databaseId, page, 10);
@@ -24,26 +25,58 @@ export default async function sitemap() {
     fetchAllPages(process.env.NOTION_DATABASE_PRODUCTS_ID as string),
   ]);
 
+  const articlesPerPage = 10;
+  const productsPerPage = 10;
+  const totalArticlePages = Math.ceil(articles.length / articlesPerPage);
+  const totalProductPages = Math.ceil(products.length / productsPerPage);
+
+  // Generate blog post URLs with pagination
   const blogSitemaps = articles.map((item, index) => {
-    const pageNumber = Math.floor(index / 10) + 1;
+    const pageNumber = Math.floor(index / articlesPerPage) + 1;
     return {
       url: `${process.env.BASE_URL}/blog/${pageNumber}/${item.post_slug}`,
       lastModified: item.modified_date,
     };
   });
 
+  // Generate store post URLs with pagination
   const storeSitemaps = products.map((item, index) => {
-    const pageNumber = Math.floor(index / 10) + 1;
+    const pageNumber = Math.floor(index / productsPerPage) + 1;
     return {
       url: `${process.env.BASE_URL}/store/${pageNumber}/${item.post_slug}`,
       lastModified: item.modified_date,
     };
   });
 
+  // Generate URLs for each page in the blog section
+  const blogPageSitemaps = Array.from(
+    { length: totalArticlePages },
+    (_, index) => ({
+      url: `${process.env.BASE_URL}/blog/${index + 1}`,
+      lastModified: new Date().toISOString(),
+    })
+  );
+
+  // Generate URLs for each page in the store section
+  const storePageSitemaps = Array.from(
+    { length: totalProductPages },
+    (_, index) => ({
+      url: `${process.env.BASE_URL}/store/${index + 1}`,
+      lastModified: new Date().toISOString(),
+    })
+  );
+
+  // Generate static routes
   const routes = sitemapList.pages.map((item) => ({
     url: `${process.env.BASE_URL}${item.path}`,
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes, ...blogSitemaps, ...storeSitemaps];
+  return [
+    ...routes,
+    ...blogSitemaps,
+    ...storeSitemaps,
+    ...blogPageSitemaps,
+    ...storePageSitemaps,
+  ];
 }
