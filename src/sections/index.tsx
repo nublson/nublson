ffff2a461e9b9c1a29527b4d8eb99f7fba03f3bd
Nubelson Fernandes <me@nublson.com>
2025-04-23 21:45:13 +0100

    fix: remove undefined export

--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -2,6 +2,5 @@ export * from "./Content";
 export * from "./Gears";
 export * from "./Header";
 export * from "./LastPosts";
-export * from "./Posts";
 export * from "./Share";
 export * from "./Work";
