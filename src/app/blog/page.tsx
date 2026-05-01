6974c9c3676e9deae148bfc702c202195d60a8bf
Nubelson Fernandes <nubelsondev@gmail.com>
2026-05-01 17:09:43 +0100

    feat: add base Blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,19 @@
+import Hero from "@/sections/hero";
+import PostsSection from "@/sections/posts";
+import { Fragment } from "react";
+
 export default function BlogPage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      <p>Blog</p>
-    </div>
+    <Fragment>
+      <Hero
+        title="My Blog"
+        description="Here’s where I share stories from the road, reflections on creativity, and insights from the ever-evolving adventure of building, exploring, and growing. Whether it's lessons from the wild or thoughts sparked by late-night brainstorming sessions, this space captures the journey in motion."
+        thumbnail={{
+          src: "https://avatar.vercel.sh/nublson",
+          alt: "Blog Thumbnail",
+        }}
+      />
+      <PostsSection title="Latest Posts" id="blog" />
+    </Fragment>
   );
 }
